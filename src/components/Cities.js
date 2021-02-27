1fbffd7830e03502fba7dcec477b0188e2ce3cd3
Daniel Alejandro Cast <lexcast13@gmail.com>
2021-02-26 23:59:10 -0600

    Improve cities and travels lists

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -9,18 +9,20 @@ const Cities = ({ cities, onRemove }) => {
       <span className="text-xs font-medium mb-1 mt-2">
         CITIES <span className="text-gray-400">({cities.length})</span>
       </span>
-      <div className="max-h-24 bg-gray-800 rounded-lg overflow-y-auto">
+      <div className="text-xs max-h-24 bg-gray-800 rounded-lg overflow-y-auto">
         {cities.map((c) => (
           <div
-            onClick={() => onRemove(c.geonameId)}
             key={c.geonameId}
-            className="group cursor-pointer px-3 py-1 flex items-center hover:bg-gray-700"
+            className="group px-3 py-1 flex items-center hover:bg-gray-700"
           >
             {c.countryCode && (
-              <span className="mr-2">{emoji(c.countryCode)}</span>
+              <span className="mr-2 flex-shrink-0">{emoji(c.countryCode)}</span>
             )}
             <span className="flex-1">{c.name}</span>
-            <span className="group-hover:visible invisible text-xs">
+            <span
+              onClick={() => onRemove(c.geonameId)}
+              className="cursor-pointer group-hover:visible invisible text-xs"
+            >
               <FontAwesomeIcon icon={faTimes} />
             </span>
           </div>
--- src/components/Travels.js
+++ src/components/Travels.js
@@ -17,12 +17,11 @@ const Travels = ({ travels, onRemove }) => {
       <span className="text-xs font-medium mb-1 mt-2">
         TRAVELS <span className="text-gray-400">({travels.length})</span>
       </span>
-      <div className="max-h-24 bg-gray-800 rounded-lg overflow-y-auto">
+      <div className="text-xs max-h-24 bg-gray-800 rounded-lg overflow-y-auto">
         {travels.map((t) => (
           <div
-            onClick={() => onRemove(t.id)}
             key={t.id}
-            className="group cursor-pointer px-3 py-1 flex items-center hover:bg-gray-700"
+            className="group cursor-pointer px-3 py-2 flex items-center hover:bg-gray-700"
           >
             <FontAwesomeIcon
               className="text-xs mr-2 text-gray-400"
@@ -32,7 +31,9 @@ const Travels = ({ travels, onRemove }) => {
               {["start", "end"].map((i) => (
                 <div key={i} className="flex items-center">
                   {t[i].countryCode && (
-                    <span className="mr-2">{emoji(t[i].countryCode)}</span>
+                    <span className="mr-2 flex-shrink-0">
+                      {emoji(t[i].countryCode)}
+                    </span>
                   )}
                   <span className="flex-1">{t[i].name}</span>
                   {i === "start" && (
@@ -44,7 +45,10 @@ const Travels = ({ travels, onRemove }) => {
                 </div>
               ))}
             </div>
-            <span className="group-hover:visible invisible text-xs">
+            <span
+              onClick={() => onRemove(t.id)}
+              className="group-hover:visible invisible text-xs"
+            >
               <FontAwesomeIcon icon={faTimes} />
             </span>
           </div>
