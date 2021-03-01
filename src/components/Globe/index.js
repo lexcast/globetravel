2cc04977e26feee8dfbac8be90dd14332a910b08
Daniel Alejandro Cast <lexcast13@gmail.com>
2021-02-28 22:19:06 -0600

    Use orthographic camera

--- src/components/Globe/index.js
+++ src/components/Globe/index.js
@@ -9,7 +9,7 @@ import Curve from "./Curve";
 
 const Globe = ({ cities, travels }) => {
   return (
-    <Canvas camera={{ position: [0, 0, 200] }}>
+    <Canvas orthographic camera={{ position: [0, 0, 200] }}>
       <Earth />
       <Land />
       {cities.map((city) => (
