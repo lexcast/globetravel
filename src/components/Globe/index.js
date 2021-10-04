ec6b2e51e3dbfd1beb6b1466484eeacb547966dd
Daniel Alejandro Cast <lexcast13@gmail.com>
2021-10-04 03:26:08 -0500

    Remove unused vars

--- src/components/Globe/index.js
+++ src/components/Globe/index.js
@@ -7,29 +7,22 @@ import Marker from "./Marker";
 import Path from "./Path";
 import Curve from "./Curve";
 
-const Globe = ({ cities, travels }) => {
-  const date = new Date();
-  const timeZoneOffset = date.getTimezoneOffset() || 0;
-  const timeZoneMaxOffset = 60 * 12;
-  const rotation = [0, Math.PI * (timeZoneOffset / timeZoneMaxOffset), 0];
-
-  return (
-    <Canvas orthographic camera={{ position: [0, 0, 200], zoom: 3 }}>
-      <Earth />
-      <Land />
-      {cities.map((city) => (
-        <Marker key={city.geonameId} city={city} />
-      ))}
-      {travels.map((travel) =>
-        travel.type === "flight" ? (
-          <Curve key={travel.id} travel={travel} />
-        ) : (
-          <Path key={travel.id} travel={travel} />
-        )
-      )}
-      <OrbitControls enablePan enableRotate autoRotate />
-    </Canvas>
-  );
-};
+const Globe = ({ cities, travels }) => (
+  <Canvas orthographic camera={{ position: [0, 0, 200], zoom: 3 }}>
+    <Earth />
+    <Land />
+    {cities.map((city) => (
+      <Marker key={city.geonameId} city={city} />
+    ))}
+    {travels.map((travel) =>
+      travel.type === "flight" ? (
+        <Curve key={travel.id} travel={travel} />
+      ) : (
+        <Path key={travel.id} travel={travel} />
+      )
+    )}
+    <OrbitControls enablePan enableRotate autoRotate />
+  </Canvas>
+);
 
 export default Globe;
